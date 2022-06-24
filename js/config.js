c34eb6ad0eadeb09f3bce85a49b01a566573eb92
Alexander Green <alexander.green@logius.nl>
2022-06-24 16:44:41 +0200

    Initial upload

---
+++ js/config.js
@@ -0,0 +1,78 @@
+var respecConfig =
+{
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "dk",
+  shortName: "template",
+  publishDate: "2022-06-24",
+  publishVersion: "0.0.1",
+  // previousPublishVersion: "(none)",
+  //  previousPublishDate: "(none)",
+  //  previousMaturity: "WV",
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/API-Design-Rules",
+  
+  
+ // Controls if linked "§" section markers are added to a document
+ addSectionLinks: true,
+ 
+    // this parameter will add the tag_name of the latest release to the document Title
+  // only set this parameter when a release has been set
+  nl_addReleaseTagTitle: true,
+
+    // nl_organisationName is used for some company specific values in the header (and Sotd)
+  // currently supported: Logius and Geonovum (default)  
+  nl_organisationName: "Logius",
+  
+  // this url points to the folder where organsation specific css files are stored
+  // defaults to https://tools.geostandaarden.nl/respec/style/ if not set
+  nl_organisationStylesURL: "https://publicatie.centrumvoorstandaarden.nl/respec/style/",
+
+  // prefix for the names of company specific css, svg and ico prefixes
+  // defaults to "GN-"  
+  nl_organisationPrefix: "LS-",
+
+  // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
+  // so that figures can be linked are be a part of table of figures
+  nl_markdownEmbedImageInFigure: true,
+
+  // nl_organisationPublishURL points to organisation specifica publication page, used in header
+  // defaults to  https://docs.geostandaarden.nl/"
+  nl_organisationPublishURL: "https://publicatie.centrumvoorstandaarden.nl/",
+
+  // nl_logo refers to company logo
+  // defaults to https://tools.geostandaarden.nl/respec/style/logos/Geonovum.svg
+  nl_logo: {
+    src: "https://publicatie.centrumvoorstandaarden.nl/respec/style/logos/figure-logius.svg",
+    alt: "Logius",
+    id: "Logius",
+    height: 77,
+    width: 44,
+    url: "https://www.logius.nl/standaarden",
+  },
+
+
+  localBiblio: {
+    "SemVer": {
+      href: "https://semver.org",
+      title: "Semantic Versioning 2.0.0",
+      authors: ["T. Preston-Werner"],
+      date: "June 2013"
+    }
+  },
+};
