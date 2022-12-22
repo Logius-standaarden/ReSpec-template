ab28ee3918a193cd7317eda1919fb564254b8dc5
Alexander Green <alexander.green@logius.nl>
2022-12-22 09:31:28 +0100

    New build (#30)

--- js/config.js
+++ js/config.js
@@ -1,52 +1,40 @@
 var respecConfig = {
-    //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
-    specStatus: "WV",
-    //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
-    specType: "HR",
-    pubDomain: "dk",
-    shortName: "template",
-    publishDate: "2022-06-24",
-    publishVersion: "0.0.1",
-    title: "titleText",
-    // previousPublishVersion: "(none)",
-    //  previousPublishDate: "(none)",
-    //  previousMaturity: "WV",
-    content: {"ch01": "informative", "ch02": ""},
-    editors:
-        [
-            {
-                name: "Logius Standaarden",
-                company: "Logius",
-                companyURL: "https://github.com/Logius-standaarden",
-            }
-        ],
-    authors:
-        [
-            {
-                name: "Logius Standaarden",
-                company: "Logius",
-                companyURL: "https://github.com/Logius-standaarden",
-            }
-        ],
-    github: "https://github.com/Logius-standaarden/API-Design-Rules",
+  useLogo: true,
+  useLabel: true,
+  license: "cc0",
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "dk",
+  shortName: "template",
+  publishDate: "2022-06-24",
+  publishVersion: "0.0.1",
+  title: "titleText",
+  // previousPublishVersion: "(none)",
+  content: {"ch01": "informative", "ch02": ""},
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/ReSpec-template",
 
 
-    // Controls if linked "§" section markers are added to a document
-    addSectionLinks: true,
-
-    // this parameter will add the tag_name of the latest release to the document Title
-    // only set this parameter when a release has been set
-    nl_addReleaseTagTitle: true,
-
-    // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
-    // so that figures can be linked are be a part of table of figures
-    nl_markdownEmbedImageInFigure: true,
-
-    // Create PDF and link to file in header:
-    // alternateFormats: [
-    //     {
-    //         label: "pdf",
-    //         uri: "rename-me.pdf",
-    //     },
-    // ],
+  // Create PDF and link to file in header:
+  // alternateFormats: [
+  //     {
+  //         label: "pdf",
+  //         uri: "rename-me.pdf",
+  //     },
+  // ],
 };
--- js/organisation-config.js
+++ js/organisation-config.js
@@ -1,31 +1,151 @@
+// Plaats dit bestand op een centrale locatie voor hergebruik in meerdere documenten.
 var organisationConfig = {
-    // nl_organisationName is used in the header (and Sotd)
-    nl_organisationName: "Logius",
+  nl_organisationName: "Logius",
+  nl_organisationStylesURL:
+    "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
+  nl_organisationPublishURL: "https://gitdocumentatie.logius.nl/publicatie/",
+  logos: [{
+    src: "https://gitdocumentatie.logius.nl/publicatie/respec/style/logos/figure-logius.svg",
+    alt: "Logius",
+    id: "Logius",
+    height: 77,
+    width: 44,
+    url: "https://www.logius.nl/standaarden",
+  }],
+  
+  addSectionLinks: true,
 
-    // this url points to the folder where organsation specific css files are stored
-    // defaults to https://tools.geostandaarden.nl/respec/style/ if not set
-    nl_organisationStylesURL: "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
-
-    // prefix for the names of company specific css, svg and ico prefixes
-    // defaults to "GN-"
-    nl_organisationPrefix: "LS-",
-
-    // nl_organisationPublishURL points to organisation specifica publication page, used in header
-    // defaults to  https://docs.geostandaarden.nl/"
-    nl_organisationPublishURL: "https://gitdocumentatie.logius.nl/publicatie/",
-
-    // nl_logo refers to company logo
-    // defaults to https://tools.geostandaarden.nl/respec/style/logos/Geonovum.svg
-    nl_logo: {
-        src: "https://gitdocumentatie.logius.nl/publicatie/respec/style/logos/figure-logius.svg",
-        alt: "Logius",
-        id: "Logius",
-        height: 77,
-        width: 44,
-        url: "https://www.logius.nl/standaarden",
+  localizationStrings: {
+    en: {
+      wv: "Draft",
+      cv: "Recommendation",
+      vv: "Proposed recommendation",
+      def: "Definitive version",
+      basis: "Document",
+      eo: "Outdated version",
+      tg: "Rescinded version",
+      no: "Norm",
+      st: "Standard",
+      im: "Information model",
+      pr: "Guideline",
+      hr: "Guide",
+      wa: "Proposed recommendation",
+      al: "General",
+      bd: "Governance documentation",
+      bp: "Best practice",
     },
+    nl: {
+      wv: "Werkversie",
+      cv: "Consultatieversie",
+      vv: "Versie ter vaststelling",
+      def: "Vastgestelde versie",
+      basis: "Document",
+      eo: "Verouderde versie",
+      tg: "Teruggetrokken versie",
+      no: "Norm",
+      st: "Standaard",
+      im: "Informatiemodel",
+      pr: "Praktijkrichtlijn",
+      hr: "Handreiking",
+      wa: "Werkafspraak",
+      al: "Algemeen",
+      bd: "Beheerdocumentatie",
+      bp: "Best practice",
+    },
+  },
+
+  specTypeText: {
+    en: {
+      no: "Norm",
+      st: "Standard",
+      im: "Information model",
+      pr: "Guideline",
+      hr: "Guide",
+      wa: "Proposed recommendation",
+      al: "General",
+      bd: "Governance documentation",
+      bp: "Best practice",
+    },
+    nl: {
+      no: "Norm",
+      st: "Standaard",
+      im: "Informatiemodel",
+      pr: "Praktijkrichtlijn",
+      hr: "Handreiking",
+      wa: "Werkafspraak",
+      al: "Algemeen",
+      bd: "Beheerdocumentatie",
+      bp: "Best practice",
+    },
+  },
+
+  specStatusText: {
+    en: {
+      wv: "Draft",
+      cv: "Recommendation",
+      vv: "Proposed recommendation",
+      def: "Definitive version",
+      basis: "Document",
+      eo: "Outdated version",
+      tg: "Rescinded version",
+    },
+    nl: {
+      wv: "Werkversie",
+      cv: "Consultatieversie",
+      vv: "Versie ter vaststelling",
+      def: "Vastgestelde versie",
+      basis: "Document",
+      eo: "Verouderde versie",
+      tg: "Teruggetrokken versie",
+    },
+  },
+
+  sotdText: {
+    nl: {
+      sotd: "Status van dit document",
+      def: `Dit is de definitieve versie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd.`,
+      wv: `Dit is een werkversie die op elk moment kan worden gewijzigd, verwijderd of vervangen door andere documenten. Het is geen door het TO goedgekeurde consultatieversie.`,
+      cv: `Dit is een door het TO goedgekeurde consultatieversie. Commentaar over dit document kan gestuurd worden naar `,
+      vv: `Dit is een definitief concept van de nieuwe versie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd.`,
+      basis: "Dit is een document zonder officiële status.",
+    },
+    en: {
+      sotd: "Status of This Document",
+      def: `This is the definitive version of this document. Edits resulting from consultations have been applied.`,
+      wv: `This is a draft that could be altered, removed or replaced by other documents. It is not a recommendation approved by TO.`,
+      cv: `This is a proposed recommendation approved by TO. Comments regarding this document may be sent to `,
+      vv: `This is the definitive concept of this document. Edits resulting from consultations have been applied.`,
+      basis: "This document has no official standing.",
+    },
+  },
+
+  labelColor: {
+    def: "#154273",
+    wv: "#32a852",
+  },
+  licenses: {
+    cc0: {
+      name: "Creative Commons 0 Public Domain Dedication",
+      short: "CC0",
+      url: "https://creativecommons.org/publicdomain/zero/1.0/",
+      image:
+        "https://tools.geostandaarden.nl/respec/style/logos/CC-Licentie.svg",
+    },
+    "cc-by": {
+      name: "Creative Commons Attribution 4.0 International Public License",
+      short: "CC-BY",
+      url: "https://creativecommons.org/licenses/by/4.0/legalcode",
+      image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by.svg",
+    },
+    "cc-by-nd": {
+      name: "Creative Commons Naamsvermelding-GeenAfgeleideWerken 4.0 Internationaal",
+      short: "CC-BY-ND",
+      url: "https://creativecommons.org/licenses/by-nd/4.0/legalcode.nl",
+      image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by-nd.svg",
+    },
+  },
 	
-	localBiblio: {
+  localBiblio: {
 	"SemVer": {
             href: "https://semver.org",
             title: "Semantic Versioning 2.0.0",
